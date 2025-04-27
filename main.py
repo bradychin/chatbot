922ebc1465520fb0d491b30c6da4ddf917d0cc23
Brady <brady@bradychin.dev>
2025-04-26 23:14:09 -0500

    Working Chatbot
    Needs more fine tuning. Better training speed. Clean up

--- main.py
+++ main.py
@@ -1,19 +1,55 @@
 #--------- 1. Import libraries ---------#
 from torch.optim import AdamW
-from torch.utils.data import DataLoader, TensorDataset
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from torch.utils.data import DataLoader, Dataset
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, get_linear_schedule_with_warmup
 import torch
+import logging
 
-#--------- 2. Process Data ---------#
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+#--------- Tokenize ---------#
+class DialogManagement(Dataset):
+    def __init__(self, dialog_pairs, tokenizer, max_length=512):
+        self.dialog_pairs = dialog_pairs
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.dialog_pairs)
+
+    def __getitem__(self, idx):
+        prompt, response = self.dialog_pairs[idx]
+        # Format: <BOS> prompt <EOS> response <EOS>
+        combined_text = f"{self.tokenizer.bos_token} {prompt} {self.tokenizer.eos_token} {response} {self.tokenizer.eos_token}"
+
+        encodings = self.tokenizer(combined_text,
+                                   truncation=True,
+                                   max_length=self.max_length,
+                                   padding="max_length",
+                                   return_tensors="pt")
+
+        input_ids = encodings["input_ids"].squeeze()
+        attention_mask = encodings["attention_mask"].squeeze()
+
+        # For GPT training, labels are the same as inputs
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "labels": input_ids
+        }
+
+#--------- Process Data ---------#
 class ProcessData:
     def __init__(self, movie_lines_path, movie_conversations_path):
         self.movie_lines_path = movie_lines_path
         self.movie_conversations_path = movie_conversations_path
         self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        special_tokens = {'pad_token': '<PAD>', 'bos_token': '<BOS>', 'eos_token': '<EOS>'}
+        self.tokenizer.add_special_tokens(special_tokens)
         self.lines = self.load_lines()
         self.conversations = self.load_conversations()
         self.dialog = self.create_dialog()
-        self.dialog_tokens = self.convert_to_tokens()
 
     # Import lines
     def load_lines(self):
@@ -45,49 +81,49 @@ class ProcessData:
                 dialog.append((prompt, response))
         return dialog
 
-    def convert_to_tokens(self):
-        dialog_tokens = []
-        for pair in self.dialog:
-            input_token = self.tokenizer.encode(pair[0], return_tensors='pt', max_length=1024, truncation=True)
-            output_token = self.tokenizer.encode(pair[1], return_tensors='pt', max_length=1024, truncation=True)
-            print(f'Input: {input_token.shape}')
-            print(f'Output: {output_token.shape}')
-            concat_token = torch.cat((input_token, output_token), dim=1)
-            print('Concat success')
-            dialog_tokens.append(concat_token)
-        return dialog_tokens
+    def get_dataset(self):
+        return DialogManagement(self.dialog, self.tokenizer)
 
-#--------- 3. Chatbot ---------#
+#--------- Chatbot ---------#
 class Chatbot:
     def __init__(self, movie_lines_path, movie_conversations_path):
         self.data_processor = ProcessData(movie_lines_path, movie_conversations_path)
-        self.pretrained_model = GPT2LMHeadModel.from_pretrained('gpt2')
+        self.tokenizer = self.data_processor.tokenizer
+        self.model = GPT2LMHeadModel.from_pretrained('gpt2')
+        self.model.resize_token_embeddings(len(self.tokenizer))
 
     def fine_tune(self):
-        print('fine tune function')
-        input_ids = torch.cat(self.data_processor.dialog_tokens, dim=0)
-        print('input_ids success')
-        dataset = TensorDataset(input_ids)
+        dataset = self.data_processor.get_dataset()
         dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-        print('dataloader success')
-        optimizer = AdamW(self.pretrained_model.parameters(), lr=5e-6)
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        logger.info(f'Using device: {device}')
+
+        self.model.to(device)
+        optimizer = AdamW(self.model.parameters(), lr=5e-6)
 
         # Fine tune
-        self.pretrained_model.train()
-        print('train_success')
+        self.model.train()
         epochs = 3
         for epoch in range(epochs):
-            for batch in dataloader:
-                inputs = batch[0]
-                outputs = self.pretrained_model(input_ids=inputs, labels=inputs)
+            for batch_index, batch in enumerate(dataloader):
+                input_ids = batch['input_ids'].to(device)
+                attention_mask = batch['attention_mask'].to(device)
+                labels = batch['labels'].to(device)
+                if input_ids.size(0) == 0:
+                    print('Empty batch. Skipping')
+                    continue
+                outputs = self.model(input_ids = input_ids,
+                                     attention_mask = attention_mask,
+                                     labels = labels)
                 loss = outputs.loss
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
                 print(f'Epoch: {epoch}. Loss: {loss.item()}')
 
-        self.pretrained_model.save_pretrained("fine_tuned_chatbot")
-        self.data_processor.tokenizer.save_pretrained("fine_tuned_chatbot")
+        # self.model.save_pretrained("fine_tuned_chatbot")
+        # self.data_processor.tokenizer.save_pretrained("fine_tuned_chatbot")
 
 #--------- Main Function ---------#
 def main():
